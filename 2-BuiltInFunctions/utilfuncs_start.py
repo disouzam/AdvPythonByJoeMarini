759f52bd8a511dbfb1f9cca59b60f33a0a030334
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-28 18:05:00 -0300

    feat: Filled the TODOs using any, all, min, max and sum from built-in functions

--- 2-BuiltInFunctions/utilfuncs_start.py
+++ 2-BuiltInFunctions/utilfuncs_start.py
@@ -4,15 +4,20 @@
 def main():
     # use any() and all() to test sequences for boolean values
     list1 = [1, 2, 3, 0, 5, 6]
-    
-    # TODO: any will return true if any of the sequence values are true
-    
-    # TODO: all will return true only if all values are true
-    
-    # TODO: min and max will return minimum and maximum values in a sequence
-    
-    # TODO: Use sum() to sum up all of the values in a sequence
-    
+
+    # any will return true if any of the sequence values are true
+    print(any(list1))
+
+    # all will return true only if all values are true
+    print(all(list1))
+
+    #  min and max will return minimum and maximum values in a sequence
+    print("min: ", min(list1))
+    print("max: ", max(list1))
+
+    # Use sum() to sum up all of the values in a sequence
+    print("sum: ", sum(list1))
+
+
 if __name__ == "__main__":
     main()
-    
