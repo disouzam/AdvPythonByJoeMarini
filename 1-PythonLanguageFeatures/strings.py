f930f7f5a9e71b21bc5aa9308af665481006812c
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-27 22:11:08 -0300

    feat: Introduced a file to play with strings

---
+++ 1-PythonLanguageFeatures/strings.py
@@ -0,0 +1,25 @@
+"""Module to play with strings vs bytes and template strings lectures"""
+# Content copied from Ex_Files_Adv_Python\01 Language\strings_start.py
+
+# strings and bytes are not directly interchangeable
+# strings contain unicode, bytes are raw 8-bit values
+
+
+def main():
+    # define some starting values
+    b = bytes([0x41, 0x42, 0x43, 0x44])
+    print(b)
+
+    s = "This is a string"
+    print(s)
+
+    # TODO: Try combining them.
+
+    # TODO: Bytes and strings need to be properly encoded and decoded
+    # before you can work on them together
+
+    # TODO: encode the string as UTF-32
+
+
+if __name__ == "__main__":
+    main()
